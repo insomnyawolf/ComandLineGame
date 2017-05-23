1f0ef1c02e938f4d780b53a2f644746928f35bfd
insomnyawolf <ruben.tokyo1999@gmail.com>
2017-05-23 16:29:04 +0200

    Finally a decent Outputwriter class
    Code Improvement

--- ComandLineGame/Game/OutputWriter.cs
+++ ComandLineGame/Game/OutputWriter.cs
@@ -1,59 +1,59 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ConsoleGame.Game
 {
     class OutputWriter
     {
-        static string outputline1, outputline2, outputline3, outputline4, outputline5, outputline6, outputline7, outputline8, outputline9, outputline10;
+        static List<string> outputbuffer = new List<string>();
         static string outputline;
+        static int outformat;
 
         public static void ClearLines()
         {
-            outputline1 = outputline2 = outputline3 = outputline4 = outputline5 = outputline6 = outputline7 = outputline8 = outputline9 = outputline10 = String.Empty;
+            outputbuffer.Clear();
         }
 
         public static void Output()
         {
-            //This works but needs a rework
-            outputline1 = outputline2;
-            outputline2 = outputline3;
-            outputline3 = outputline4;
-            outputline4 = outputline5;
-            outputline5 = outputline6;
-            outputline6 = outputline7;
-            outputline7 = outputline8;
-            outputline8 = outputline9;
-            outputline9 = "        " + outputline10;
-            outputline10 = Game.output;
-            outputline =
-                outputline1 +
-                Environment.NewLine +
-                outputline2 +
-                Environment.NewLine +
-                outputline3 +
-                Environment.NewLine +
-                outputline4 +
-                Environment.NewLine + 
-                outputline5 +
-                Environment.NewLine + 
-                outputline6 +
-                Environment.NewLine + 
-                outputline7 +
-                Environment.NewLine +
-                outputline8 +
-                Environment.NewLine +
-                outputline9 +
-                Environment.NewLine +
-                "========" + outputline10 + "========";
             ClearCurrentConsole();
+
+            if (outputbuffer.ElementAtOrDefault(9) != null)
+            {
+                outputbuffer.RemoveAt(0);
+            }
+            else
+            {
+                for(int x = 0; x < 9; x++)
+                {
+                    outputbuffer.Add(String.Empty);
+                }    
+            }
+            outputbuffer.Add(Game.output);
+            foreach (string thing in outputbuffer)
+            {
+                if(outformat <= 8)
+                {
+                    outputline += Environment.NewLine + "        " + thing + "        ";
+                    outformat++;
+                }
+                else
+                {
+                    outputline += Environment.NewLine + "========" + thing + "========";
+                    outformat = 0;
+                }
+            }
             Console.WriteLine(outputline);
+            outputline = null;
         }
 
         public static void ClearCurrentConsole()
         {
             int currentLineCursor = ++Console.CursorTop;
             int targetLine = currentLineCursor - 100;
-            while (currentLineCursor > targetLine  && currentLineCursor > 3)
+            while (currentLineCursor > targetLine && currentLineCursor > 3)
             {
                 Console.SetCursorPosition(0, currentLineCursor);
                 Console.Write(new string(' ', Console.WindowWidth));
