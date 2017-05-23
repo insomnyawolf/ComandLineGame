a5ac7006d7f4545ea00b6856f6bc86103404444e
insomnyawolf <ruben.tokyo1999@gmail.com>
2017-05-23 16:27:32 +0200

    Finally a decent Outputwriter class
    Code improvement

---
+++ Info.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace ConsoleGame.Game
+{
+    class Info
+    {
+        public static void Intro()
+        {
+            Console.WriteLine(
+                "Press Ctrl + C To Stop" + Environment.NewLine +
+                "Default keys are: Q,W,E,R" + Environment.NewLine +
+                "Press P key to start without timmer or" + Environment.NewLine +
+                "any other key to start the default mode" + Environment.NewLine);
+        }
+    }
+}
---
+++ OutputWriter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleGame.Game
+{
+    class OutputWriter
+    {
+        static List<string> outputbuffer = new List<string>();
+        static string outputline;
+        static int outformat;
+
+        public static void ClearLines()
+        {
+            outputbuffer.Clear();
+        }
+
+        public static void Output()
+        {
+            ClearCurrentConsole();
+
+            if (outputbuffer.ElementAtOrDefault(9) != null)
+            {
+                outputbuffer.RemoveAt(0);
+            }
+            else
+            {
+                for(int x = 0; x < 9; x++)
+                {
+                    outputbuffer.Add(String.Empty);
+                }    
+            }
+            outputbuffer.Add(Game.output);
+            foreach (string thing in outputbuffer)
+            {
+                if(outformat <= 8)
+                {
+                    outputline += Environment.NewLine + "        " + thing + "        ";
+                    outformat++;
+                }
+                else
+                {
+                    outputline += Environment.NewLine + "========" + thing + "========";
+                    outformat = 0;
+                }
+            }
+            Console.WriteLine(outputline);
+            outputline = null;
+        }
+
+        public static void ClearCurrentConsole()
+        {
+            int currentLineCursor = ++Console.CursorTop;
+            int targetLine = currentLineCursor - 100;
+            while (currentLineCursor > targetLine && currentLineCursor > 3)
+            {
+                Console.SetCursorPosition(0, currentLineCursor);
+                Console.Write(new string(' ', Console.WindowWidth));
+                Console.SetCursorPosition(0, currentLineCursor);
+                currentLineCursor--;
+            }
+        }
+    }
+}
